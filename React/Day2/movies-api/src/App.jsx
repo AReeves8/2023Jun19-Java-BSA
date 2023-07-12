abf8ad2c93707d189eb69743316eaa429371c470
AReeves8 <areeves@skillstorm.com>
2023-07-12 13:05:40 -0500

    trussworks and react http requests

---
+++ React/Day2/movies-api/src/App.jsx
@@ -0,0 +1,12 @@
+import '@trussworks/react-uswds/lib/index.css';
+import Movies from './components/Movies/Movies';
+
+
+export default function App() {
+
+    return(
+        <>
+            <Movies />
+        </>
+    );
+}
